d7c7cebacdb88f5d2a1e178d95fe108f9309b78d
Francisnasc <38249727+Fjulio2018@users.noreply.github.com>
2024-05-23 10:27:50 -0300

    first commit

---
+++ src/test/java/br/com/automation/testNG/DemonstraTest.java
@@ -0,0 +1,23 @@
+package br.com.automation.testNG;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class DemonstraTest {
+    WebDriver driver;
+    @BeforeTest
+    public void setup(){
+
+
+
+
+    }
+
+    @Test
+    public void validaHome(){
+        Assert.assertEquals(1,2);
+
+    }
+}
