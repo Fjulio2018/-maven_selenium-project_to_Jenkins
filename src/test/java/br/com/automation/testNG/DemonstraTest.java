749a3c9259e41dd243d37314d04265659e87d46d
Francisnasc <38249727+Fjulio2018@users.noreply.github.com>
2024-05-23 11:07:01 -0300

    Open the HomePage

--- src/test/java/br/com/automation/testNG/DemonstraTest.java
+++ src/test/java/br/com/automation/testNG/DemonstraTest.java
@@ -1,23 +1,26 @@
 package br.com.automation.testNG;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class DemonstraTest {
     WebDriver driver;
+
     @BeforeTest
-    public void setup(){
-
-
+    public void setup() {
+        driver = new FirefoxDriver();
+        driver.navigate().to("https://teststore.automationtesting.co.uk/index.php/");
+        driver.manage().window().maximize();
 
 
     }
 
     @Test
-    public void validaHome(){
-        Assert.assertEquals(1,1);
+    public void validaHome() {
+        Assert.assertEquals(1, 1);
 
     }
 }
