6d2c963de7e0368ad6005af585b3e8fadce83e2f
Francisnasc <38249727+Fjulio2018@users.noreply.github.com>
2024-05-23 13:59:03 -0300

    DemonstraTest modify

--- src/test/java/br/com/automation/testNG/DemonstraTest.java
+++ src/test/java/br/com/automation/testNG/DemonstraTest.java
@@ -5,53 +5,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.slf4j.Logger;
 import static java.lang.invoke.MethodHandles.lookup;
 import static org.slf4j.LoggerFactory.getLogger;
-import org.slf4j.Logger;
 
 public class DemonstraTest {
     WebDriver driver;
     static final Logger log = getLogger(lookup().lookupClass());
 
-    @BeforeSuite
-    public void setUpClass(){
-        // Define o caminho do ChromeDriver manualmente
-        System.setProperty("webdriver.chrome.driver", "/var/jenkins_home/workspace/Pipe Mvn Project/maven_selenium-project/chromedriver-win64/chromedriver.exe");
-
-
-        // Opcional: configura o ChromeDriver automaticamente, se necessário
-        // WebDriverManager.chromedriver().setup();
-    }
-
     @BeforeTest
     public void setup() {
+        // Configura o ChromeDriver usando o WebDriverManager
+        WebDriverManager.chromedriver().setup();
+
+        // Inicializa o WebDriver
         driver = new ChromeDriver();
-        // Maximiza a janela do navegador
         driver.manage().window().maximize();
     }
 
     @Test
     public void validaHome() {
         String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/";
-        try {
-            driver.get(sutUrl);
-            String currentTitle = driver.getTitle();
-            String expectedTitle = "Selenium WebDriver";
-            log.debug("The title of {} is {}", sutUrl, currentTitle);
-            Assert.assertTrue(currentTitle.contains(expectedTitle), "The Title is like that: " + currentTitle);
-        } catch (Exception e) {
-            log.error("An error occurred while validating home page: {}", e.getMessage());
-            Assert.fail("An error occurred while validating home page: " + e.getMessage());
-        }
+        driver.get(sutUrl);
+        String currentTitle = driver.getTitle();
+        String expectedTitle = "Selenium WebDriver";
+        log.debug("The title of {} is {}", sutUrl, currentTitle);
+        Assert.assertTrue(currentTitle.contains(expectedTitle), "The Title is like that: " + currentTitle);
     }
 
     @AfterTest
     void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        driver.quit();
     }
 }
